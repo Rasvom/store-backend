49e264356b7bf972454885d53e826f9161a8dcfa
Rasvom <rasvomb@gmail.com>
2023-12-08 01:10:42 +0300

    update library, install cookie-parser

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require("cookie-parser")
 const mongoose = require("mongoose");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
@@ -11,6 +12,7 @@ const io = new Server(httpServer, { cors: "http://localhost:5173" });
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }));
 app.use(require("./routes/user.route"));
 
